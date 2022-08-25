21f8d757dd45a7f63c27969ebe3aa25365fc22bc
YesMan <kaka0804@naver.com>
2022-08-26 05:54:15 +0900

    update: TableData.Main.cs

--- Assets/Scripts/TableData/TableData.Main.cs
+++ Assets/Scripts/TableData/TableData.Main.cs
@@ -6,7 +6,6 @@ public partial class TableData : MonoBehaviour
 {
     public class MainData
     {
-        public string communication_id;
         public string string_id;
         public int string_type;
         public int value_1;
@@ -17,7 +16,8 @@ public partial class TableData : MonoBehaviour
         public string answer2_connect_id;
         public string conv_connect_id;
     }
-    Dictionary<string, List<MainData>> mainDataDic = new Dictionary<string, List<MainData>>();
+
+    Dictionary<string, Dictionary<string, List<MainData>>> mainDataDic = new Dictionary<string, Dictionary<string, List<MainData>>>();
 
     void MainDataInit()
     {
@@ -27,9 +27,10 @@ public partial class TableData : MonoBehaviour
         {
             MainData mainData = new MainData();
 
-            mainData.communication_id = data[i]["communication_id"].ToString();
+            string story_id = data[i]["story_id"].ToString();
+            string npc_id = data[i]["npc_id"].ToString();
+
             mainData.string_id = data[i]["string_id"].ToString();
-            mainData.string_type = int.Parse(data[i]["string_type"].ToString());
             mainData.value_1 = int.Parse(data[i]["value_1"].ToString());
             mainData.conv_type = int.Parse(data[i]["conv_type"].ToString());
             mainData.answer1_string_id = data[i]["answer1_string_id"].ToString();
@@ -38,28 +39,42 @@ public partial class TableData : MonoBehaviour
             mainData.answer2_connect_id = data[i]["answer2_connect_id"].ToString();
             mainData.conv_connect_id = data[i]["conv_connect_id"].ToString();
 
-            string npc_id = data[i]["npc_id"].ToString();
-            if (!mainDataDic.ContainsKey(npc_id))
+            if(!mainDataDic.ContainsKey(story_id))
             {
-                mainDataDic.Add(npc_id, new List<MainData>());
-                mainDataDic[npc_id].Add(mainData);
+                mainDataDic.Add(story_id, new Dictionary<string, List<MainData>>());
+                if (!mainDataDic[story_id].ContainsKey(npc_id))
+                {
+                    mainDataDic[story_id].Add(npc_id, new List<MainData>());
+                    mainDataDic[story_id][npc_id].Add(mainData);
+                }
+                else
+                {
+                    mainDataDic[story_id][npc_id].Add(mainData);
+                }
             }
             else
             {
-                mainDataDic[npc_id].Add(mainData);
+                if (!mainDataDic[story_id].ContainsKey(npc_id))
+                {
+                    mainDataDic[story_id].Add(npc_id, new List<MainData>());
+                    mainDataDic[story_id][npc_id].Add(mainData);
+                }
+                else
+                {
+                    mainDataDic[story_id][npc_id].Add(mainData);
+                }
             }
         }
     }
 
-    public Dictionary<string, List<MainData>> GetMainDataDic()
+    public Dictionary<string, Dictionary<string, List<MainData>>> GetMainDataDic()
     {
         return mainDataDic;
     }
 
-    public List<MainData> GetStringIdDataList(string npc_id)
+    public List<MainData> GetStringIdDataList(string stroy_id, string npc_id)
     {
-        List<MainData> list = new List<MainData>();
-        list = mainDataDic[npc_id];
+        List<MainData> list = mainDataDic[stroy_id][npc_id];
         return list;
     }
 }
