4eb710e1595463adb5f81c655d28f88dc0efb052
YesMan <kaka0804@naver.com>
2022-08-30 13:12:35 +0900

    update: add comment

--- Assets/Scripts/QuestItem.cs
+++ Assets/Scripts/QuestItem.cs
@@ -10,6 +10,7 @@ public class QuestItem : MonoBehaviour
         {
             QuestManager.instance.questItemNum ++;
             QuestManager.instance.questStatusText.text = string.Format("종이: {0} / 5", QuestManager.instance.questItemNum);
+            //퀘스트아이템 다 모음
             if(QuestManager.instance.questItemNum == QuestManager.instance.questItemTotalNum)
             {
                 QuestManager.instance.isComplete = true;
