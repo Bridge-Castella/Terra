5da94039269dbb372c1cc1048b94ebe329dc42ef
YesMan <kaka0804@naver.com>
2022-08-30 14:27:58 +0900

    update: option

--- Assets/Scripts/UI/Option.cs
+++ Assets/Scripts/UI/Option.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 using TMPro;
 
 public class Option : MonoBehaviour
@@ -17,15 +16,18 @@ public class Option : MonoBehaviour
     public Button quitButton;
     public Button initialButton;
     public Button cancelButton;
+    public Button audioCancelButton;
 
     public GameObject mainMenuObject;
     public GameObject logoObject;
 
-    public GameObject AudioGroup;
+    public GameObject buttonGroup;
+    public GameObject audioGroup;
 
     public GameObject popUpObject;
-    public Text popUpText;
+    public TextMeshProUGUI popUpText;
     public Button popUpYesButton;
+    public Button popUpNoButton;
 
     public TextMeshProUGUI masterVolumeNum;
     public TextMeshProUGUI bgmVolumeNum;
@@ -40,6 +42,7 @@ public class Option : MonoBehaviour
         cancelButton.onClick.AddListener(OnClickCancelButton);
 
         popUpYesButton.onClick.AddListener(LoadLoginScene);
+        popUpNoButton.onClick.AddListener(OnClickPopupNoButton);
 
         //일반 볼륨
         masterSlider.onValueChanged.AddListener(SetMasterVolume);
@@ -59,8 +62,6 @@ public class Option : MonoBehaviour
 
         sfxSlider.value = AudioManager.instance.sfxVolumePercent;
         sfxVolumeNum.text = Mathf.CeilToInt(sfxSlider.value * 10).ToString();
-
-        EventSystem.current.SetSelectedGameObject(audioButton.gameObject);
     }
 
     public void Update()
@@ -77,7 +78,7 @@ public class Option : MonoBehaviour
 
     public void OnClickQuitButton()
     {
-        AudioGroup.SetActive(false);
+        buttonGroup.SetActive(false);
         popUpObject.SetActive(true);
         popUpText.text = "게임을 종료하시겠습니까?";
     }
@@ -88,13 +89,55 @@ public class Option : MonoBehaviour
         MapManager.instance.mapState = MapManager.MapState.Login;
         SceneManager.LoadScene("01.Login");
 
-        //임시: 아이템 개수 초기화
+        //TODO 임시: 아이템 개수 초기화
         for(int i = 0; i< Inventory.instance.itemObejcts.Count; i++)
         {
             Inventory.instance.itemObejcts[i].amount = 0;
         }
     }
 
+    public void OnClickPopupNoButton()
+    {
+        popUpObject.SetActive(false);
+        buttonGroup.SetActive(true);
+    }
+
+    public void OnClickCancelButton()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+            if (MapManager.instance.mapState == MapManager.MapState.Login)
+            {
+                AudioManager.instance.PlaySound("ui_02");
+                mainMenuObject.SetActive(true);
+                logoObject.SetActive(true);
+                audioGroup.SetActive(false);
+                buttonGroup.SetActive(true);
+            }
+            else
+            {
+                if(ControlManager.instance != null)
+                {
+                    ControlManager.instance.Resume();
+                }    
+            }
+        }
+    }
+
+    public void InitailizeVolume()
+    {
+        masterSlider.value = 0.5f;
+        bgmSlider.value = 0.5f;
+        sfxSlider.value = 0.5f;
+    }
+
+    public void OnClickAudioButton()
+    {
+        buttonGroup.SetActive(false);
+        popUpObject.SetActive(false);
+        audioGroup.SetActive(true);
+    }
     public void SetBgmVolume(float value)
     {
         AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Bgm);
@@ -108,40 +151,6 @@ public class Option : MonoBehaviour
     public void SetMasterVolume(float value)
     {
         AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
-    }
-
-    public void InitailizeVolume()
-    {
-        masterSlider.value = 0.5f;
-        bgmSlider.value = 0.5f;
-        sfxSlider.value = 0.5f;
-    }
-
-    public void OnClickAudioButton()
-    {
-        popUpObject.SetActive(false);
-        AudioGroup.SetActive(true);
-    }
-
-    public void OnClickCancelButton()
-    {
-        if (gameObject.activeSelf)
-        {
-            gameObject.SetActive(false);
-            if (MapManager.instance.mapState == MapManager.MapState.Login)
-            {
-                AudioManager.instance.PlaySound("ui_02");
-                mainMenuObject.SetActive(true);
-                logoObject.SetActive(true);
-            }
-            else
-            {
-                if(ControlManager.instance != null)
-                {
-                    ControlManager.instance.Resume();
-                }    
-            }
-        }
     }
 
     public void ChangeMasterVolumeNum(float value)
