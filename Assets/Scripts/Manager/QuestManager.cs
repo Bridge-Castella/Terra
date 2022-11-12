3a56f9d86dc722ab0dc256fd49fe96fb1c05aec5
YesMan <kaka0804@naver.com>
2022-11-12 22:23:06 +0900

    update: questmanager
    -퀘스트 매니저에서 퀘스트 아이템 찾도록 함

--- Assets/Scripts/Manager/QuestManager.cs
+++ Assets/Scripts/Manager/QuestManager.cs
@@ -34,7 +34,7 @@ public class QuestManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        questItemGroup = GameObject.Find("QuestItemGroup");
     }
 
     // Update is called once per frame
