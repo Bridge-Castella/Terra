a515d2bd5cf7461f9e3f3df890c6709d9a126da7
YesMan <kaka0804@naver.com>
2022-12-02 23:49:44 +0900

    update: 01.login.unity
    changed button sprite

--- Assets/Scripts/UI/ButtonHover.cs
+++ Assets/Scripts/UI/ButtonHover.cs
@@ -6,45 +6,18 @@ using TMPro;
 
 public class ButtonHover : MonoBehaviour
 {
-    public Image[] hoverImg = new Image[2];
-    public TextMeshProUGUI buttonText;
-
     [SerializeField] AK.Wwise.Event mouseHover;
     [SerializeField] AK.Wwise.Event mouseClick;
-
-    void Start()
-    {
-        for(int i = 0; i < hoverImg.Length; i++)
-            hoverImg[i].gameObject.SetActive(false);
-        //buttonText.color = Color.grey;
-    }
 
     public void MouseHoverOn()
     {
         //AudioManager.instance.PlaySound("ui_03");                             // Outdated audio engine
         mouseHover.Post(gameObject);
-
-        for (int i = 0; i < hoverImg.Length; i++)
-            hoverImg[i].gameObject.SetActive(true);
-        //buttonText.color = Color.white;
-        buttonText.fontStyle = FontStyles.Bold;
-    }
-
-    public void MouseHoverOff()
-    {
-        for (int i = 0; i < hoverImg.Length; i++)
-            hoverImg[i].gameObject.SetActive(false);
-        //buttonText.color = Color.grey;
-        buttonText.fontStyle = FontStyles.Normal;
     }
 
     public void OnClickButton()
     {
         //AudioManaver.instance.PlaySound("ui_04");                             // Outdated audio engine
         mouseClick.Post(gameObject);
-
-        for (int i = 0; i < hoverImg.Length; i++)
-            hoverImg[i].gameObject.SetActive(false);
-        buttonText.fontStyle = FontStyles.Normal;
     }
 }
