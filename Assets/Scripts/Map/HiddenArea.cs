bf1f6f2ef7c444abb981d37c62562f0fd21802d0
YesMan <kaka0804@naver.com>
2023-01-06 14:08:27 +0900

    add: hidden area
    숨겨진 공간 추가
    플레이어 나갔다 들어오면 fade in/out 됨

---
+++ Assets/Scripts/Map/HiddenArea.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenArea : MonoBehaviour
+{
+    public float disappearRate = 1f;
+
+    private bool playerEntered;
+    private SpriteRenderer wallSprite;
+
+    float alphaValue = 1f;
+    private void Start()
+    {
+        wallSprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if(playerEntered)
+        {
+            alphaValue -= Time.deltaTime * disappearRate;
+            if(alphaValue <= 0f)
+                alphaValue = 0f;
+
+            wallSprite.color = new Color(wallSprite.color.r, wallSprite.color.g, wallSprite.color.b, alphaValue);
+        }
+        else
+        {
+            alphaValue += Time.deltaTime * disappearRate;
+            if (alphaValue >= 1f)
+                alphaValue = 1f;
+
+            wallSprite.color = new Color(wallSprite.color.r, wallSprite.color.g, wallSprite.color.b, alphaValue);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            playerEntered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerEntered = false;
+        }
+    }
+}
