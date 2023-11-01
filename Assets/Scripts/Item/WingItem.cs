3ff69a534156e929de6dda9f595dde64d1d14cda
ross1573 <ross1573@naver.com>
2023-11-01 13:13:35 +0900

    update: wing item time indicator
    QA 12

--- Assets/Scripts/Item/WingItem.cs
+++ Assets/Scripts/Item/WingItem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WingItem : Item
@@ -7,6 +8,14 @@ public class WingItem : Item
     [SerializeField] private float wingTime = 3f;
     [SerializeField] private float showTime = 2f;
     [SerializeField] GameObject itemSprite;
+    [SerializeField] GameObject timeIndicator;
+
+    private PlayerMove player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMove>();
+    }
 
     public override void GetWingItem()
     {
@@ -22,8 +31,39 @@ public class WingItem : Item
         InGameAudio.Post(InGameAudio.Instance.ITEM_Wing_01);
         InGameAudio.Post(InGameAudio.Instance.ITEM_Wing_02);
 
-        yield return new WaitForSeconds(wingTime);
+        var indicator = Instantiate(timeIndicator, player.transform);
+        var indicatorText = indicator.GetComponentInChildren<TextMeshProUGUI>();
+        var timeElasped = 0f;
+
+        while (timeElasped < wingTime)
+        {
+            timeElasped += Time.deltaTime;
+
+            var remaining = wingTime - timeElasped;
+            if (remaining < 0)
+            {
+                remaining = 0f;
+            }
+
+            if (remaining > 1.0f)
+            {
+                indicatorText.text = Mathf.CeilToInt(remaining).ToString();
+            }
+            else
+            {
+                indicatorText.text = remaining.ToString("F1");
+            }
+
+            var scale = indicator.transform.localScale;
+            scale.x = player.facingRight ? Mathf.Abs(scale.x) : Mathf.Abs(scale.x) * -1;
+            indicator.transform.localScale = scale;
+
+            yield return null;
+        }
+
         abilities.isFlying = false;
+        indicator.gameObject.SetActive(false);
+        Destroy(indicator);
         StartCoroutine(CoShowWingItem());
     }
 
