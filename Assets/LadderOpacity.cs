81598067fc632019813c303bd3996b55c68b6235
YesMan <kaka0804@naver.com>
2024-01-05 17:37:20 +0900

    사다리 수정

--- Assets/LadderOpacity.cs
+++ Assets/LadderOpacity.cs
@@ -2,65 +2,29 @@ using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class LadderOpacity : MonoBehaviour
 {
-    public float value =10.0f;
-    public float duration =300000.0f;
-    public bool bIsLadderUpdate = false;
-    public bool bFirstVisible =true;
-    public GameObject particleSystemAlert;
-    // Start is called before the first frame update
-
-    private UnityEngine.U2D.SpriteShapeRenderer r;
-    private Color c;
-    private float remainTime;
-    private float time = 0.0f;
+    private SpriteShapeRenderer renderer;
+    private Color color;
     
     void Start()
     {
-        if (GetComponentInParent<UnityEngine.U2D.SpriteShapeRenderer>())
-        {
-            r = GetComponentInParent<UnityEngine.U2D.SpriteShapeRenderer>();
-            c = new Color();
-            c = Color.white;
-            c.a = 0;
-            r.color = c;
-
-            // particleSystemAlert = transform.Find("PortalEffect").gameObject;
-
-            //StartCoroutine(LerpFunction(speed));
-        }
-
+        renderer = GetComponentInParent<SpriteShapeRenderer>();
+        color = new Color();
+        color = Color.white;
+        color.a = 0;
+        renderer.color = color;
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator CoShowLadderOpacity()
     {
-        if(bIsLadderUpdate)
+        while (color.a < 1f)
         {
-            if (time < duration)
-            {
-                c.a = Mathf.Lerp(0, 255, time / duration);
-                r.color = c;
-                time += Time.deltaTime;
-                if(time/duration >= 1)
-                {
-                    bIsLadderUpdate =false;
-                    
-                }
-            }
-        }
-    }
-
-    public void ShowLadderOpacity()
-    {
-        if(!bIsLadderUpdate)
-        {   bIsLadderUpdate = true;
-            particleSystemAlert.active = false;
-            //
-            UnityEngine.Debug.Log("player is dead");
-            //particleSystemAlert.SetActive(false);
+            color.a += Time.deltaTime;
+            renderer.color = color;
+            yield return null;
         }
     }
 }
