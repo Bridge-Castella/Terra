b4369bcad4879736fc738fb553bcf91b4596e087
ross1573 <ross1573@naver.com>
2024-02-14 23:12:41 +0900

    add: 햄버거 버튼 추가

--- Assets/Scripts/UI/GNBCanvas.cs
+++ Assets/Scripts/UI/GNBCanvas.cs
@@ -9,11 +9,13 @@ public class GNBCanvas : MonoBehaviour
     public static GNBCanvas instance;
 
     [Header("----Menu----")]
+    [SerializeField] private Button groupButton;
     [SerializeField] private Button questButton;
     [SerializeField] private Button inventoryButton;
     [SerializeField] private Button homeButton;
 
     [Header("----Popup----")]
+    [SerializeField] private GameObject groupButtonContent;
     [SerializeField] private GameObject content;
     [SerializeField] private GameObject questPanel;
     [SerializeField] private InventoryUI inventoryPanel;
@@ -76,9 +78,17 @@ public class GNBCanvas : MonoBehaviour
 
     private void Start()
     {
+        groupButton.onClick.AddListener(OnClickGroupButton);
         questButton.onClick.AddListener(OnClickQuestButton);
         inventoryButton.onClick.AddListener(OnClickInventoryButton);
         homeButton.onClick.AddListener(OnClickHomeButton);
+    }
+
+    private void OnClickGroupButton()
+    {
+        StopAllCoroutines();
+        StartCoroutine(CoWaitForAutoDisappear(3));
+        groupButtonContent.SetActive(true);
     }
 
     private void OnClickQuestButton()
@@ -102,5 +112,37 @@ public class GNBCanvas : MonoBehaviour
     public void ShowToastPopup(string path, string message)
     {
         toastPopup.Show(path, message);
+    }
+
+    IEnumerator CoWaitForAutoDisappear(float delay)
+    {
+        var images = groupButtonContent.GetComponentsInChildren<Image>();
+
+        foreach (var image in images)
+        {
+            var color = image.color;
+            color.a = 1f;
+            image.color = color;
+        }
+
+        yield return new WaitForSeconds(delay);
+
+        var timeElasped = 0f;
+        var fadeTime = 1f;
+        while (timeElasped < fadeTime)
+        {
+            timeElasped += Time.deltaTime;
+
+            foreach (var image in images)
+            {
+                var color = image.color;
+                color.a = (fadeTime - timeElasped) / fadeTime;
+                image.color = color;
+            }
+
+            yield return null;
+        }
+
+        groupButtonContent.SetActive(false);
     }
 }
