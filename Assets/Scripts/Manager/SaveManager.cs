2616e0945c6f0800e90b8bb04e07bab98305cd71
ross1573 <ross1573@naver.com>
2024-05-01 14:37:02 +0900

    fix: 퀘스트 시작 후 죽거나 게임을 종료하면 퀘스트를 재게할 수 없던 문제 수정

--- Assets/Scripts/Manager/SaveManager.cs
+++ Assets/Scripts/Manager/SaveManager.cs
@@ -85,6 +85,8 @@ public class SaveManager
     public static void SaveGame()
     {
         var data = SaveGameData();
+        if (data.mapData.index == MapManager.MapIndex.Login)
+            return;
 
         try
         {
